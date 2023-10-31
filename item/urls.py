d527472d1cd2e2fef74a05bbbf75088af74deed4
Timbusien <timbusiennavi@gmail.com>
2023-11-01 00:35:54 +0500

    last

--- item/urls.py
+++ item/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = "item"
+
 urlpatterns = [
     path('', views.index, name='homepage'),
     path('about', views.about, name='about'),
     path('create', views.create, name='create'),
+    path('register', views.register_request, name='register'),
+    path('login', views.login_request, name='login'),
+    path('logout', views.logout_request, name='logout'),
 
 ]
--- store/settings.py
+++ store/settings.py
@@ -38,9 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'item',
+    'crispy_forms',
+    'crispy_bootstrap4',
+
 
 
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
